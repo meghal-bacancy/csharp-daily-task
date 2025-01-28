92e77f793b4279bf8f9711f83590860b4ee90fdf
Meghal2103 <meghal2103@outlook.com>
2025-01-28 11:39:19 +0530

    day2

---
+++ day2/day2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Write a program that tries to open and read from a file. Make sure to use the finally block to ensure that the file is always closed properly. 
+
+using System.IO;
+
+StreamReader reader = null;
+
+try
+{
+    reader = new StreamReader("D:\\bacancy_training\\csharp-daily-task\\day2\\day2\\text_file.txt");
+    Console.WriteLine(reader.ReadToEnd());
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("The file was not found.");
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"An I/O error occurred: {ex.Message}");
+}
+finally
+{
+    if (reader != null)
+    {
+        reader.Close();
+    }
+}
